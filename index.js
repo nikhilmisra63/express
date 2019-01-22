73124f91c02b25520dfd4e4e374d49842b6684f4
Nikhil <nikhil@writingminds.com>
2019-01-22 19:09:31 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(786, () => console.log("Listen"));
+const port = process.env.PORT || 786;
+app.listen(port, () => console.log("Listen"));
